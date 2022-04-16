fb6ebfaf8fc4d47c482cf6bb14a51b00721840e4
kuldeep <kuldykld@gmail.com>
2022-04-16 16:16:46 +0530

    formik

---
+++ src/components/formik/Formikk.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FormikTutorial from "./FormikTutorial";
+import FormikWithContext from "./FormikWithContext";
+
+const Formikk = () => {
+  return (
+    <>
+      <FormikTutorial />
+      <hr />
+      <p>Formik with context</p>
+      <FormikWithContext />
+    </>
+  );
+};
+
+export default Formikk;
