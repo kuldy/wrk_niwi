c671155409fe6bf92b09d09af72f6bd9df3bf878
kuldeep <kuldykld@gmail.com>
2022-04-18 15:51:25 +0530

    hooks in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import AllFormComponents from "./components/formcomponents/AllFormComponents";
 import Formikk from "./components/formik/Formikk";
+import EffectHook from "./components/use-effect-hook/EffectHook";
 import RefHook from "./components/use-ref-hook/RefHook";
+import StateHook from "./components/use-state-hook/StateHook";
 import ReducerHook from "./components/user-reducer-hook/ReducerHook";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <AllFormComponents />
       <RefHook />
       <ReducerHook />
+      <StateHook />
+      <EffectHook />
     </div>
   );
 }
